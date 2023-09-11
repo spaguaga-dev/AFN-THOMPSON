18b18207572ee24567cee5092d5f1b45b02902ee
spaguaga-dev <144509562+spaguaga-dev@users.noreply.github.com>
2023-09-10 21:25:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
+import uvicorn
+from asgiref.wsgi import WsgiToAsgi
+
 from flask import Flask, render_template, request, redirect, url_for
 
 from modules.Gif        import crear_gif_thompson
-from modules.Thompson   import Thompson
-from modules.Graficador import graficarAutomataFinitoNoDeterminista
-from modules.Postfix    import convertirAPostfix, formatearExpresionRegular
-
-import os
+@@ -13,11 +9,10 @@
 import shutil
 
 app = Flask(__name__)
+asgi_app = WsgiToAsgi(app)
 
 @app.route('/')
 def hello_world():
@@ -16,16 +16,7 @@ def hello_world():
     gif_exists = os.path.exists(gif_path)
     return render_template('index.html', gif_exists=gif_exists)
 
-@app.route('/generate', methods=['POST'])
-def generate():
-    try:
-        user_input = request.form['user_input']
-        
-        print()
-
-        print(" + La expresion regular es:", user_input)
-
-        print(" + Generando el AFN...")
+@@ -34,7 +29,7 @@ def generate():
 
         # ELIMINANDO EL DIRECTORIO DE IMAGENES SI EXISTE
 
@@ -33,35 +24,10 @@ def generate():
         if os.path.exists(directorio):
             shutil.rmtree(directorio)
             print(f" - Directorio '{directorio}' eliminado con éxito.")
-        else:
-            print(f" - El directorio '{directorio}' no existe.")
-
-        # CREAMOS EL AUTOMATA FINITO NO DETERMINISTA
-
-        regex = user_input
-
-        # Formateamos la expresion regular para que sea valida
-
-        regex = formatearExpresionRegular(regex)
-
-        # Convertimos la expresion regular de infix a postfix
-
-        regex = convertirAPostfix(regex)
-
-        AFN_Thompson = Thompson(regex).afn
-
-        # Graficamos el automata finito no determinista
-
-        graficarAutomataFinitoNoDeterminista(AFN_Thompson)
-
-        print(" + Generando el GIF...")
-
-        # CREAMOS EL GIF
-
-        crear_gif_thompson()
-        
-        return redirect(url_for('hello_world'))
-    
+@@ -70,6 +65,3 @@ def generate():
     except Exception as e:
         print(" + Error:", e)
         return render_template('index.html', error=e)
+
+if __name__ == "__main__":
+    uvicorn.run(asgi_app, host="0.0.0.0", port=8181)
