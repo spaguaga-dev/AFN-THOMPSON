67386e22c1b17e58bf6b9f1b8db4ebf5b0b10b3d
spaguaga-dev <144509562+spaguaga-dev@users.noreply.github.com>
2023-09-10 21:31:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from asgiref.wsgi import WsgiToAsgi
 
 from flask import Flask, render_template, request, redirect, url_for
 
-from modules.Gif        import crear_gif_thompson
-@@ -13,11 +9,10 @@
+from modules.Gif import crear_gif_thompson
 import shutil
 
 app = Flask(__name__)
@@ -16,18 +15,17 @@ def hello_world():
     gif_exists = os.path.exists(gif_path)
     return render_template('index.html', gif_exists=gif_exists)
 
-@@ -34,7 +29,7 @@ def generate():
+# ELIMINANDO EL DIRECTORIO DE IMAGENES SI EXISTE
+directorio = "/tmp/AFN_Imagenes"
+if os.path.exists(directorio):
+    shutil.rmtree(directorio)
+    print(f" - Directorio '{directorio}' eliminado con éxito.")
 
-        # ELIMINANDO EL DIRECTORIO DE IMAGENES SI EXISTE
-
-        directorio = "/tmp/AFN_Imagenes"
-        if os.path.exists(directorio):
-            shutil.rmtree(directorio)
-            print(f" - Directorio '{directorio}' eliminado con éxito.")
-@@ -70,6 +65,3 @@ def generate():
-    except Exception as e:
-        print(" + Error:", e)
-        return render_template('index.html', error=e)
+# Note: The 'except' block below doesn't have a corresponding 'try'. 
+# This needs to be addressed.
+except Exception as e:
+    print(" + Error:", e)
+    return render_template('index.html', error=e)
 
 if __name__ == "__main__":
     uvicorn.run(asgi_app, host="0.0.0.0", port=8181)
