34b25c7ddca992456f17b3aaf03e97f52d20c581
spaguaga-dev <144509562+spaguaga-dev@users.noreply.github.com>
2023-09-10 21:20:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-
-import uvicorn
-from asgiref.wsgi import WsgiToAsgi
-
 from flask import Flask, render_template, request, redirect, url_for
 
 from modules.Gif        import crear_gif_thompson
@@ -13,11 +9,10 @@ import os
 import shutil
 
 app = Flask(__name__)
-asgi_app = WsgiToAsgi(app)
 
 @app.route('/')
 def hello_world():
-    gif_path = "static/RESULTADO.gif"
+    gif_path = "/tmp/RESULTADO.gif"
     gif_exists = os.path.exists(gif_path)
     return render_template('index.html', gif_exists=gif_exists)
 
@@ -34,7 +29,7 @@ def generate():
 
         # ELIMINANDO EL DIRECTORIO DE IMAGENES SI EXISTE
 
-        directorio = "AFN_Imagenes"
+        directorio = "/tmp/AFN_Imagenes"
         if os.path.exists(directorio):
             shutil.rmtree(directorio)
             print(f" - Directorio '{directorio}' eliminado con éxito.")
@@ -70,6 +65,3 @@ def generate():
     except Exception as e:
         print(" + Error:", e)
         return render_template('index.html', error=e)
-    
-if __name__ == "__main__":
-    uvicorn.run(asgi_app, host="0.0.0.0", port=8181)
